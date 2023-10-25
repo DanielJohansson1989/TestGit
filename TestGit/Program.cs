8ba7d598ef0ec228641542a4984406d897e77b33
Jonathanjohansson24 <jonathan.nordin24@gmail.com>
2023-10-25 15:44:15 +0200

    added text

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BIlar bilar = new BIlar();
+            Flygplan flygplan = new Flygplan();
+
+            Fordon[] fordon = { flygplan, bilar };
+            
+            foreach (Fordon ford in fordon)
+            {
+                ford.Go();
+            }
         }
     }
 }
