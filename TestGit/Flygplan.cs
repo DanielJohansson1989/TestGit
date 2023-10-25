61e3ddfbab26e40dea32c767e863fe1e2b6d6aa4
Jonathanjohansson24 <jonathan.nordin24@gmail.com>
2023-10-25 15:19:23 +0200

    h

--- TestGit/Flygplan.cs
+++ TestGit/Flygplan.cs
@@ -11,9 +11,6 @@ namespace TestGit
         public string Name { get; set; }
         public string Type { get; set; }
 
-        public void Flying()
-        {
-            Console.WriteLine("the plane is flying");
-        }
+     
     }
 }
