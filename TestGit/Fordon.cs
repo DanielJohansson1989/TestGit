9ca1a8fd4e6cb65606d699f4a1f085385d49986b
Daniel Johansson <dannesnake@gmail.com>
2023-10-25 15:18:44 +0200

    ny class fordon

---
+++ TestGit/Fordon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    internal class Fordon
+    {
+        public virtual void Go()
+        {
+            Console.WriteLine();
+        }
+    }
+}
